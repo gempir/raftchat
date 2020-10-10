7641a18a810ad1046025d79d60c604e1b4578bbf
gempir <gempir.dev@gmail.com>
2020-10-10 15:54:49 +0200

    Revert "add linter"
    This reverts commit 71cedd9b873d6303c938cf6278d2ef6effe672bf.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import React from "react";
-import { Split } from "./layout/Split";
+import React from 'react';
+import './App.css';
 
-export function App(): JSX.Element {
+export function App() {
     return (
         <div className="App">
-            <Split />
+            <header className="App-header">
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload. I did. Great.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
         </div>
     );
 }
--- src/layout/Split.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export function Split(): JSX.Element {
-    return <div>split</div>;
-}
