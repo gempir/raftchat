08b53bde78049b052e9326bf42098a1906d4423c
gempir <gempir.dev@gmail.com>
2020-10-10 17:06:13 +0200

    add colors variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.css";
+import { Menu } from "./layout/Menu";
 import { Tab } from "./layout/Tab";
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
 
 export function App(): JSX.Element {
     return (
-        <div className="App">
+        <AppContainer className="App">
+            <Menu />
             <Tab />
-        </div>
+        </AppContainer>
     );
 }
--- src/layout/Menu.tsx
+++ src/layout/Menu.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import styled from "styled-components";
+import { colors } from "../variables/colors";
+
+const MenuContainer = styled.div`
+    display: flex;
+    width: 100%;
+    height: 2rem;
+    background: ${colors.bgBright}
+`;
 
 export function Menu(): JSX.Element {
-    return <div>
+    return <MenuContainer>
         Settings
-    </div>
+    </MenuContainer>
 }
