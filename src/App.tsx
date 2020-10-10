71cedd9b873d6303c938cf6278d2ef6effe672bf
gempir <gempir.dev@gmail.com>
2020-10-10 15:47:18 +0200

    add linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Split } from "./layout/Split";
 
-export function App() {
+export function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload. I did. Great.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <Split />
         </div>
     );
 }
---
+++ src/layout/Split.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Split(): JSX.Element {
+    return <div>split</div>;
+}
