688a7a9c406355a5491081c260fae666a2c5c989
gempir <gempir@users.noreply.github.com>
2020-10-10 12:07:09 +0000

    Automated deployment: Sat Oct 10 12:07:09 UTC 2020 428de7d6833f4e8aaeadba759aa8b41c5f2ecb79

---
+++ precache-manifest.50eea10d6bc2c617a9ec456bf2597e08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a2f52475b9aedfa2a38b645ee68cb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f4089977fbe5c623b2e",
+    "url": "/static/css/main.7d693b32.chunk.css"
+  },
+  {
+    "revision": "1ff33d829b90c1f213f2",
+    "url": "/static/js/2.174e2544.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.174e2544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f4089977fbe5c623b2e",
+    "url": "/static/js/main.4c999b5f.chunk.js"
+  },
+  {
+    "revision": "89443013b0e1f49c49cd",
+    "url": "/static/js/runtime-main.dfb1d6ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
