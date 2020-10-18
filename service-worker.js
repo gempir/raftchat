e9bf796795d06906e3b3ee9397a982a9b496ef3a
gempir <gempir@users.noreply.github.com>
2020-10-18 15:59:29 +0000

    Automated deployment: Sun Oct 18 15:59:29 UTC 2020 809cd17190610981c29f7db51de87050e4e1df2b

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b2f708b5b23cdbf67c2a9e6e7d1ef304.js"
+  "/precache-manifest.bed820f1a3c1dc988815f4e80399570b.js"
 );
 
 self.addEventListener('message', (event) => {
